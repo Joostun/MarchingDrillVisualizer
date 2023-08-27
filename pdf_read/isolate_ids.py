978744d154904df6126cad18f092d677ddc33731
Joostun <justin12yu@gmail.com>
2023-08-26 20:52:59 -0400

    added seperating code

--- pdf_read/isolate_ids.py
+++ pdf_read/isolate_ids.py
@@ -1,11 +1,29 @@
 import re
 
+marcher_dict = {}
+
 with open("text_output.txt") as file:
     data = file.read()
 
+split_data = data.split("Printed:")
+
 pattern1 = r"Symbol: (\w+)"
 pattern2 = r"Label: (\w+)"
-matches1 = re.findall(pattern1, data)
-matches2 = re.findall(pattern2, data)
-print(matches1)
-print(matches2)
+
+for i in split_data:
+    section = re.findall(pattern1, i)
+    label = re.findall(pattern2, i)
+    if label == "":
+        label = 0
+    name = f"{section}{label}"
+    temp_dict = {
+        "section": section,
+        "label": label,
+        "dot": i
+    }
+    marcher_dict[name] = temp_dict
+
+for x in marcher_dict:
+    print (x)
+    for y in marcher_dict[x]:
+        print (y,':',marcher_dict[x][y]) 
