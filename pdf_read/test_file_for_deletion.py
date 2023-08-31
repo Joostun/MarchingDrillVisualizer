dcc94202b0b5fcdda6901a2e30fb8bcb5ab5be53
Joostun <justin12yu@gmail.cm>
2023-08-31 09:17:32 -0400

    working x coordinate finder

--- pdf_read/test_file_for_deletion.py
+++ pdf_read/test_file_for_deletion.py
@@ -1,14 +1,20 @@
 import re
 
 def LeftSearch(fstring):
-    return re.findall(r'\b(\w+)\s+steps\b', fstring)
+    return re.findall(r'(?<=:\s).+(?= steps)', fstring)
 
 def FindYardLine(fstring):
     return re.findall(r'\b(\w+)\s+yd\b', fstring)
 
 def XCoord(fstring):
     yard_line = int(FindYardLine(fstring)[0]) 
-    step_adjust = int(LeftSearch(fstring)[0])*0.625
+    if 'steps' in fstring:
+        step_adjust = float(LeftSearch(fstring)[0])*0.625
+    else:
+        step_adjust = 0
+    print(yard_line)
+    print(step_adjust)
+    print('.')
     translated_yard_line = 50 - yard_line
     if 'inside' in fstring:
         from_fifty = translated_yard_line - step_adjust
@@ -23,7 +29,6 @@ re_value = re.findall(":.+","1    0Side 2: On 45 yd ln 8.0 steps behind Front si
 split_list = re_value[0].split("ln")
 
        
-print(XCoord("1    0Side 2: On 45 yd ln 8.0 steps behind Front side line"))
-print(XCoord('24Side 1: 2.0 steps inside 35 yd ln 12.0 steps in front of Front Hash (HS)'))
-print(XCoord('18   32Side 2: 0.25 steps outside 50 yd ln 10.25 steps in front of Front Hash (HS)'))
-print
+print(XCoord("1    0Side 2: On 45 yd"))
+print(XCoord('24Side 1: 2.0 steps inside 35 yd'))
+print(XCoord('18   32Side 2: 0.25 steps outside 50 yd'))
