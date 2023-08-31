9ef565ea7d994a6de72fd807c20c27b313145b69
Joostun <justin12yu@gmail.cm>
2023-08-31 15:32:20 -0400

    renamed temporary file

--- pdf_read/XCoord.py
+++ pdf_read/XCoord.py
@@ -1,5 +1,8 @@
 import re
 
+class XCoordinateFinder:
+    def __init__(self, fstring):
+        self.fstring
 def LeftSearch(fstring):
     return re.findall(r'(?<=:\s).+(?= steps)', fstring)
 
@@ -7,21 +10,31 @@ def FindYardLine(fstring):
     return re.findall(r'\b(\w+)\s+yd\b', fstring)
 
 def XCoord(fstring):
+    #find yard line referenced in the instructions
     yard_line = int(FindYardLine(fstring)[0]) 
+
+    #if the dot is not exactly on the line, record the step adjustment
     if 'steps' in fstring:
         step_adjust = float(LeftSearch(fstring)[0])*0.625
     else:
         step_adjust = 0
-    print(yard_line)
-    print(step_adjust)
-    print('.')
     translated_yard_line = 50 - yard_line
+
+    #factor in the steps inside/outside the yard line
     if 'inside' in fstring:
         from_fifty = translated_yard_line - step_adjust
     elif 'outside' in fstring:
         from_fifty = translated_yard_line - step_adjust
     else:
         from_fifty = translated_yard_line
+
+    #accounts for side in position.
+    if 'Side 1' in fstring:
+        from_fifty = -abs(from_fifty)
+    else:
+        from_fifty = abs(from_fifty)
+
+    #returns x coordinate
     return from_fifty
 
 
