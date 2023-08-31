ff42fe534854cedde19b9abf1bf077d1cd8919e0
Joostun <justin12yu@gmail.com>
2023-08-31 07:19:58 -0400

    experimenting with X coordinate finder in yards

--- pdf_read/test_file_for_deletion.py
+++ pdf_read/test_file_for_deletion.py
@@ -1,13 +1,26 @@
 import re
+
+def LeftSearch(fstring):
+    return re.findall(r'\b(\w+)\s+steps\b', fstring)
+
+def FindYardLine(fstring):
+    return re.findall(r'\b(\w+)\s+yd\b', fstring)
+
+def XCoord(fstring):
+    yard_line = int(FindYardLine(fstring)[0]) 
+    step_adjust = int(LeftSearch(fstring)[0])*0.625
+    translated_yard_line = 50 - yard_line
+    if 'inside' in fstring:
+        from_fifty = translated_yard_line - step_adjust
+    elif 'outside' in fstring:
+        from_fifty = translated_yard_line - step_adjust
+    else:
+        from_fifty = translated_yard_line
+    return from_fifty
+
+
 re_value = re.findall(":.+","1    0Side 2: On 45 yd ln 8.0 steps behind Front side line")
 split_list = re_value[0].split("ln")
 
-find_number = re.findall(r'\d+', split_list[0])
-
-if 'inside' in split_list[0]:
-    print("contains inside")
-else if 'outside' in split_list[0]:
-    print("contains outside")
-
-                      
-print(find_number)
+       
+print(XCoord("1    0Side 2: On 45 yd ln 8.0 steps behind Front side line"))
