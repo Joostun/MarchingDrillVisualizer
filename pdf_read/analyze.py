aa4b3f74f4c59f4bfa88af2dbd56ae852e899f3d
Joostun <justin12yu@gmail.com>
2023-09-10 15:54:21 -0400

    Adding frontend

--- pdf_read/analyze.py
+++ pdf_read/analyze.py
@@ -1,33 +1,36 @@
-from flask import Flask, jsonify
+from flask import Flask, request
 from Marcher import Marcher
+from PyPDF2 import PdfReader
+import json
 
 marcher_list= []
 
-with open("text_output.txt") as f:
-    raw = f.read()
-
-sheets = raw.split("Printed:")[:-1]
-
-for sheet in sheets:
-    marcher_list.append(Marcher(sheet))
-
-print(marcher_list[0].name)
-print(marcher_list[0].counts)
-print(marcher_list[0].x)
-print(marcher_list[0].y)
-
 marcherAPI = Flask(__name__)
 
-@marcherAPI.route('/api/retrieveMarcher/<int:marcherNumber>', methods=['GET'])
-def retrieveMarcher(marcherNumber):
-    return jsonify({
-        'name': marcher_list[marcherNumber].name,
-        'counts': marcher_list[marcherNumber].counts,
-        'x': marcher_list[marcherNumber].x,
-        'y': marcher_list[marcherNumber].y,
-    })
+@marcherAPI.route('/api/retrieveMarcher', methods=['POST'])
+def retrieveMarcher():
+    if request.method == 'POST':
+        marcher_objects= []
+
+        raw = request.form.get('key')
+        reader = PdfReader("raw")
+        page = reader.pages[0]
+        rawtext = page.extract_text()
+
+        sheets = rawtext.split("Printed:")[:-1]
+
+        for sheet in sheets:
+            marcher_list.append(Marcher(sheet))
+            marcher_index = sheets.index(sheet)
+            marcher_dict = {
+            'name': marcher_list[marcher_index].name,
+            'counts': marcher_list[marcher_index].counts,
+            'x': marcher_list[marcher_index].x,
+            'y': marcher_list[marcher_index].y,
+            }
+            marcher_objects.append(marcher_dict)
+
+    return json.dumps(marcher_objects,indent = 6)
 
 if __name__ == '__main__':
-    marcherAPI.run(debug=True)
-
-print(retrieveMarcher(0))
+    marcherAPI.run(debug=True)  
--- web/assets/scripts.js
+++ web/assets/scripts.js
@@ -1,4 +1,32 @@
-/*let canvas = document.getElementById(screen);
-let ctx = canvas.getContext('2d');*/
+const fileInput = document.getElementById("fileInput");
+const uploadButton = document.getElementById("uploadButton");
+const statusMessage = document.getElementById("statusMessage");
+// Add an event listener to handle the file upload when the button is clicked
+uploadButton.addEventListener("click", () => {
+    const selectedFile = fileInput.files[0];
+    if (selectedFile) {
+        // Create a FormData object to send the file
+        const formData = new FormData();
+        formData.append("pdfFile", selectedFile, selectedFile.name);
 
-console.log(fetch("http://localhost:5000/api/retrieveMarcher/0"))
+        // Make a POST request to your Flask server
+        fetch("http://localhost:5000/api/retrieveMarcher", {
+            method: "POST",
+            body: formData,
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            // Display a success message or handle the response data as needed
+            statusMessage.textContent = "PDF uploaded successfully!";
+        })
+        .catch(error => {
+            // Handle any errors that occurred during the fetch
+            statusMessage.textContent = `Error: ${error.message}`;
+        });
+    }
+});
